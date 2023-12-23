a84063ab327985b0c5d2b47f29f481825a3881f4
Essada <adamessawi04@gmail.com>
2023-12-23 13:25:46 -0500

    Initial Commit

---
+++ College Market V.2/College_Market_V_2App.swift
@@ -0,0 +1,17 @@
+//
+//  College_Market_V_2App.swift
+//  College Market V.2
+//
+//  Created by Adam Essawi on 12/23/23.
+//
+
+import SwiftUI
+
+@main
+struct College_Market_V_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ College Market V.2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  College Market V.2
+//
+//  Created by Adam Essawi on 12/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
